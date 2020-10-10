05ba8d1bbe85a8a6a977bea7e7cfdad18784421d
codepipe <47555726+codepipe@users.noreply.github.com>
2020-10-10 11:55:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ Name_likit: <input type="text" name="name"><br>
 E-mail-email: <input type="text" name="email"><br>
 Password: <input type="text" name="email"><br>
 Mobile_number: <input type="text" name="email"><br>
+SNS_REVIEW: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
