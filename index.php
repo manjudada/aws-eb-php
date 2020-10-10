840eccc3ac39c8ad19a2ccc97c778427a970b78c
codepipe <47555726+codepipe@users.noreply.github.com>
2020-10-10 10:49:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <body>
 
 <form action="welcome.php" method="post">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
+Name_likit: <input type="text" name="name"><br>
+E-mail-email: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
