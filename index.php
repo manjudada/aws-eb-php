6b318936029278a6a48923120f52333c4b965c97
codepipe <47555726+codepipe@users.noreply.github.com>
2020-10-24 12:41:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 
 <form action="welcome.php" method="post">
 Name_likit: <input type="text" name="name"><br>
-E-mail-email: <input type="text" name="email"><br>
-Password: <input type="text" name="email"><br>
-Mobile_number: <input type="text" name="email"><br>
-SNS_REVIEW: <input type="text" name="email"><br>
+E-mail-email: <input type="prabhu" name="email"><br>
+Password: <input type="shiva" name="email"><br>
+Mobile_number: <input type="sindhu" name="email"><br>
+aws: <input type="text" name="email"><br>
+aws_devops: <input type="text" name="email"><br>
 <input type="submit">
 </form>
 
