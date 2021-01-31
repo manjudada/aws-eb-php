85e1b664173a47c4df94f1ed5f27209a1b78dbfb
manjudada <78254147+manjudada@users.noreply.github.com>
2021-01-31 09:54:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 Name_likit: <input type="text" name="name"><br>
 E-mail-email: <input type="manju" name="email"><br>
 Password: <input type="punith" name="email"><br>
-Mobile_number: <input type="chetan" name="email"><br>
+Mobile_number: <9663781904="chetan" name="email"><br>
 jay: <input type="text" name="email"><br>
 madhu_dada : <input type="text" name="email"><br>
 <input type="submit">
